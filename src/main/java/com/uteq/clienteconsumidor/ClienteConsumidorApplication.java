c2c6486a452be48c8fd0a1e8ed723a504d57ed6c
Freddy Farinango <freddy@gmail.com>
2026-03-02 18:33:22 -0500

    primer commit de consumidor

---
+++ src/main/java/com/uteq/clienteconsumidor/ClienteConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.uteq.clienteconsumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteConsumidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteConsumidorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uteq/clienteconsumidor/ClienteConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uteq.clienteconsumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteConsumidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
