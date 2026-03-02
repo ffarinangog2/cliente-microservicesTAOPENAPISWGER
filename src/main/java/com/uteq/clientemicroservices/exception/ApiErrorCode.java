a305639b7b83bdb61becadc2283744d104193ffe
Freddy Vladimir Farinango Guandinango <ffarinangog2@uteq.edu.ec>
2026-03-02 18:43:41 -0500

    Delete src directory

--- src/main/java/com/uteq/clientemicroservices/exception/ApiErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.uteq.clientemicroservices.exception;
-
-public enum ApiErrorCode {
-    VALIDATION_ERROR,
-    CLIENT_NOT_FOUND,
-    EMAIL_DUPLICATE,
-    DB_ERROR,
-    INTERNAL_ERROR
-}
